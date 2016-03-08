113aaa853376f02005cb76ebff79a6827efc395a
Mattias Cibien <mattias@mattiascibien.net>
2016-03-08 17:50:23 +0100

    Initial app layout.

--- app/src/main/java/net/mattiascibien/mattiascibienweb/MainActivity.java
+++ app/src/main/java/net/mattiascibien/mattiascibienweb/MainActivity.java
@@ -4,6 +4,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -14,13 +19,28 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import net.mattiascibien.mattiascibienweb.fragments.BlogFragment;
+import net.mattiascibien.mattiascibienweb.fragments.GalleryFragment;
+
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+    private ViewPager viewPager;
+
+    private PagerAdapter mPagerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        viewPager = (ViewPager) findViewById(R.id.pager);
+
+
+        mPagerAdapter = new ScreenSlidePagerAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(mPagerAdapter);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -39,8 +59,23 @@ public class MainActivity extends AppCompatActivity
         drawer.setDrawerListener(toggle);
         toggle.syncState();
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        final NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setCheckedItem(R.id.nav_camera);
         navigationView.setNavigationItemSelectedListener(this);
+
+        viewPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
+            public void onPageSelected(int page) {
+                switch (page)
+                {
+                    case 0:
+                        navigationView.setCheckedItem(R.id.nav_camera);
+                        break;
+                    case 1:
+                        navigationView.setCheckedItem(R.id.nav_gallery);
+                        break;
+                }
+            }
+        });
     }
 
     @Override
@@ -84,13 +119,35 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         if (id == R.id.nav_camera) {
-            // Handle the camera action
+            viewPager.setCurrentItem(0);
         } else if (id == R.id.nav_gallery) {
-
+            viewPager.setCurrentItem(1);
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    private class ScreenSlidePagerAdapter extends FragmentStatePagerAdapter {
+
+        private ArrayList<Fragment> fragments;
+
+        public ScreenSlidePagerAdapter(FragmentManager fm) {
+            super(fm);
+            fragments = new ArrayList<>();
+            fragments.add(new BlogFragment());
+            fragments.add(new GalleryFragment());
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return fragments.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return fragments.size();
+        }
     }
 }
---
+++ app/src/main/java/net/mattiascibien/mattiascibienweb/fragments/BlogFragment.java
@@ -0,0 +1,48 @@
+package net.mattiascibien.mattiascibienweb.fragments;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import net.mattiascibien.mattiascibienweb.R;
+
+
+public class BlogFragment extends Fragment {
+
+    private  SwipeRefreshLayout swipeContainer;
+
+     public BlogFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_blog, container, false);
+
+        swipeContainer = (SwipeRefreshLayout) view.findViewById(R.id.swipeContainer);
+        // Setup refresh listener which triggers new data loading
+        swipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                // Your code to refresh the list here.
+                // Make sure you call swipeContainer.setRefreshing(false)
+                // once the network request has completed successfully.
+                swipeContainer.setRefreshing(false);
+            }
+        });
+
+        return view;
+    }
+}
